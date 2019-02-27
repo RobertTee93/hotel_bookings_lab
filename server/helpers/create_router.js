c057d3cbd5059acf1a014e505ba42c1c207aedf9
Robert Templeton <roberttee1993@googlemail.com>
2019-02-27 11:09:24 +0000

    get all bookings route tested and working

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,7 +1,26 @@
+const express = require("express");
+const ObjectID = require("mongodb").ObjectID;
+
 
 const createRouter = function(collection){
+
+  const router = express.Router();
+
+  router.get("/", (req, res) => {
+    collection
+    .find()
+    .toArray()
+    .then(results => res.json(results))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  })
+
+  return router
 
 }
 
 
-module.exports = createRouter
+module.exports = createRouter;
